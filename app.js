38e4e5acc3a2481934f256e159ac9b8aca8008f2
ankush-anonymous <canankushdoit@gmai.com>
2024-12-19 01:04:01 +0530

    init 3

--- app.js
+++ app.js
@@ -9,15 +9,18 @@ app.use(cors());
 app.use(express.json());
 
 app.post("/proxy", async (req, res) => {
-  const googleAppsScriptUrl =
-    "https://script.google.com/macros/s/AKfycbzzq4AlfUonWPyG3cR_I5RaazicD4bVB_uxPHx83t-lX0Z4vdK8d4jJt1QSu4qiaQ/exec";
+  const googleAppsScriptUrl = process.env.APPSCRIPT_URL;
+  console.log("Received request body:", req.body); // Log incoming request
+
   try {
     const response = await axios.post(googleAppsScriptUrl, req.body, {
       headers: { "Content-Type": "application/json" },
     });
+    console.log("Apps Script response:", response.data); // Log Apps Script response
     res.json(response.data);
   } catch (error) {
-    res.status(500).send(error.message);
+    console.error("Error forwarding to Apps Script:", error.message);
+    res.status(500).send("Error forwarding request.");
   }
 });
 
